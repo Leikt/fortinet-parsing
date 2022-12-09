b29034516061c15599fdfdf05c7b9f07b82cd505
rlioret <robin.lioret@capgemini.com>
2022-12-09 09:28:00 +0100

    V0.1.0

--- fortinet_reporter/parser.py
+++ fortinet_reporter/parser.py
@@ -1,3 +1,4 @@
+from functools import partial
 from typing import Any
 
 from .stream_handler import StreamHandler
@@ -25,7 +26,7 @@ def parse_unset(context: dict, stream: StreamHandler) -> None:
             del context[name]
 
 
-def parse_config(context: dict, stream: StreamHandler) -> None:
+def parse_block(context: dict, stream: StreamHandler, end_keyword: str) -> None:
     path = stream.tokens_to_eol()
     for key in path:
         context[key] = context.get(key, dict())
@@ -34,24 +35,7 @@ def parse_config(context: dict, stream: StreamHandler) -> None:
 
     while stream.next():
         stream.skip_comments()
-        if stream.token() == 'end':
-            break
-
-        keyword = stream.token_choices(KEYWORDS)
-        SUB_PARSERS[keyword](context, stream)
-    stream.next()  # skip end keyword
-
-
-def parse_edit(context: dict, stream: StreamHandler) -> None:
-    path = stream.tokens_to_eol()
-    for key in path:
-        context[key] = context.get(key, dict())
-        context = context[key]
-    stream.expect_eol()
-
-    while stream.next():
-        stream.skip_comments()
-        if stream.token() == 'next':
+        if stream.token() == end_keyword:
             break
 
         keyword = stream.token_choices(KEYWORDS)
@@ -62,7 +46,7 @@ def parse_edit(context: dict, stream: StreamHandler) -> None:
 SUB_PARSERS = {
     'set': parse_set,
     'unset': parse_unset,
-    'config': parse_config,
-    'edit': parse_edit
+    'config': partial(parse_block, end_keyword='end'),
+    'edit': partial(parse_block, end_keyword='next')
 }
 KEYWORDS = list(SUB_PARSERS.keys())
