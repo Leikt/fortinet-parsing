4be0a7d0029df43dad235067d2b64d0ed26c1a6b
rlioret <robin.lioret@capgemini.com>
2022-12-09 12:17:24 +0100

    Add excel matrice generation

---
+++ fortiweb_report.py
@@ -0,0 +1,139 @@
+import xlsxwriter
+from xlsxwriter.worksheet import Worksheet
+
+FORMAT_HEADERS = {"align": "center", "valign": "center", "bold": True, "bg_color": "#4287f5", "font_color": "white",
+                  "text_wrap": True}
+FORMAT_ELEMENT = {"align": "center", "valign": "left", "text_wrap": True}
+
+
+def generate(data: dict, output_file: str):
+    workbook = xlsxwriter.Workbook(output_file)
+    formats = _setup_formats(workbook)
+    for ws in WORKSHEETS:
+        name = ws['name']
+        selector = ws['selector']
+        setup = ws['setup']
+        _create_worksheet(selector(data), workbook, formats, name, setup)
+    workbook.close()
+
+
+def _get_value(data, key) -> str:
+    if key in data:
+        return ', '.join(data[key])
+    return ''
+
+
+def _setup_formats(workbook):
+    return {
+        'header': workbook.add_format(FORMAT_HEADERS),
+        'element': workbook.add_format(FORMAT_ELEMENT)
+    }
+
+
+def _setup_columns(worksheet: Worksheet, columns, formatting):
+    for x, col in enumerate(columns):
+        worksheet.set_column(x, x, col[1])
+    headers = [col[0] for col in columns]
+    worksheet.write_row(0, 0, headers, formatting)
+
+
+def _create_worksheet(data, workbook, formats, sheet_name, setup):
+    worksheet = workbook.add_worksheet(sheet_name)
+    _setup_columns(worksheet, setup, formats['header'])
+    keys = [h[2] for h in setup]
+    for y, d in enumerate(data.items(), start=1):
+        name, values = d
+        row_data = []
+        for key in keys:
+            if key is None:
+                row_data.append(name)
+                continue
+
+            row_data.append(_get_value(values, key))
+        worksheet.write_row(y, 0, row_data, formats['element'])
+
+
+WS_ADDRESSES = [
+    ('Name', 40, None),
+    ('Type', 10, 'type'),
+    ('Sub-Type', 10, 'sub-type'),
+    ('Associated Interface', 15, 'associated-interface'),
+    ('Start IP', 15, 'start-ip'),
+    ('End IP', 15, 'end-ip'),
+    ('Subnet', 30, 'subnet'),
+    ('FQDN', 30, 'fqdn'),
+    ('Comment', 60, 'comment')
+]
+
+WS_POLICIES = [
+    ('Id', 5, None),
+    ('Name', 40, 'name'),
+    ('Infra source', 15, 'srcintf'),
+    ('Infra destination', 15, 'dstintf'),
+    ('Source Addresses', 60, 'srcaddr'),
+    ('Destination Addresses', 60, 'dstaddr'),
+    ('Action', 10, 'action'),
+    ('Schedule', 10, 'schedule'),
+    ('Service', 30, 'service'),
+    ('Log Traffic', 10, 'logtraffic'),
+    ('NAT', 10, 'nat'),
+    ('UTM Status', 15, 'utm-status'),
+    ('Webfilter Profile', 15, 'webfilter-profile'),
+    ('Send Deny Packet', 15, 'send-deny-packet'),
+    ('SSL SSH Profile', 20, 'ssl-ssh-profile'),
+    ('IPS Sensor', 15, 'ips-sensor'),
+    ('Application list', 15, 'application-list'),
+    ('IP Pool', 15, 'ippool'),
+    ('Pool Name', 15, 'poolname'),
+    ('Status', 15, 'status'),
+    ('Inspection Mode', 15, 'inspection-mode'),
+    ('Comment', 60, 'comments')
+]
+
+WS_SERVICE_CATEGORIES = [
+    ('Name', 30, None),
+    ('Description', 60, 'comment')
+]
+
+WS_SERVICES = [
+    ('Name', 25, None),
+    ('Category', 25, 'category'),
+    ('TCP Portrange', 20, 'tcp-portrange'),
+    ('UDP Portrange', 20, 'udp-portrange'),
+    ('Protocol', 10, 'protocol'),
+    ('Proxy', 10, 'proxy'),
+    ('ICMP Type', 10, 'icmptype')
+]
+
+WS_SERVICES_GROUPS = [
+    ('Name', 30, None),
+    ('Members', 60, 'member')
+]
+
+WORKSHEETS = [
+    {
+        'name': 'Addresses',
+        'setup': WS_ADDRESSES,
+        'selector': lambda d: d['firewall']['address']
+    },
+    {
+        'name': 'Policies',
+        'setup': WS_POLICIES,
+        'selector': lambda d: d['firewall']['policy']
+    },
+    {
+        'name': 'Service Categories',
+        'setup': WS_SERVICE_CATEGORIES,
+        'selector': lambda d: d['firewall']['service']['category']
+    },
+    {
+        'name': 'Services',
+        'setup': WS_SERVICES,
+        'selector': lambda d: d['firewall']['service']['custom']
+    },
+    {
+        'name': 'Services Groups',
+        'setup': WS_SERVICES_GROUPS,
+        'selector': lambda d: d['firewall']['service']['group']
+    }
+]
--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import json
 from pathlib import Path
 
 import fortiweb_conf_parser
+import fortiweb_report
 
 
-def main(config_file: str, output: str):
+def main(config_file: str, xlxs_output: str, json_output: str):
     source_code = Path(config_file).read_text()
     stream = fortiweb_conf_parser.StreamHandler(source_code)
     data: dict = fortiweb_conf_parser.parse(stream)
-    with open(output, 'w') as file:
+    with open(json_output, 'w') as file:
         json.dump(data, file)
+    fortiweb_report.generate(data, xlxs_output)
 
 
 if __name__ == "__main__":
-    main('data/C8ASPFIRACC.conf', 'data/parsed.json')
+    main('data/C8ASPFIRACC.conf', 'data/matrice.xlsx', 'data/data.json')
