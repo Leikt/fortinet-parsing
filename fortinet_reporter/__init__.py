4df4ab62aee5c0b63228db8efbb76dcb0b99a6b3
rlioret <robin.lioret@capgemini.com>
2022-12-09 09:33:45 +0100

    V0.1.1

---
+++ fortinet_reporter/__init__.py
@@ -0,0 +1,2 @@
+from .parser import parse
+from .stream_handler import StreamHandler
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import json
 from pathlib import Path
 
-from fortinet_reporter.parser import parse
-from fortinet_reporter.stream_handler import StreamHandler
+import fortinet_reporter
 
 source_code = Path('data/C8ASPFIRACC.conf').read_text()
-stream = StreamHandler(source_code)
-data: list = parse(stream)
+stream = fortinet_reporter.StreamHandler(source_code)
+data: list = fortinet_reporter.parse(stream)
 with open('data/parsed.json', 'w') as file:
     json.dump(data, file)
