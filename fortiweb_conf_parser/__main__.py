24cae26f4db1c64436a09a85f386353101430b59
rlioret <robin.lioret@capgemini.com>
2022-12-12 12:20:28 +0100

    Cleanup and add __main__.py to parser

---
+++ fortiweb_conf_parser/__main__.py
@@ -0,0 +1,18 @@
+import argparse
+import json
+from pathlib import Path
+
+from fortiweb_conf_parser.parser import parse
+from fortiweb_conf_parser.stream_handler import StreamHandler
+
+parser = argparse.ArgumentParser(prog="Fortinet configuration parser")
+parser.add_argument('config', type=Path,
+                    help='Path to the configuration file to parse')
+parser.add_argument('dest', type=Path,
+                    help='Path to the output json file')
+args = parser.parse_args()
+
+source_code = args.config.read_text()
+stream = StreamHandler(source_code)
+data = parse(stream)
+args.dest.write_text(json.dumps(data))
