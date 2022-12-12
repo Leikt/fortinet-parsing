b6d5c1938f4c1bc4d94dc0b89d739b006d6d89da
rlioret <robin.lioret@capgemini.com>
2022-12-12 12:14:03 +0100

    Cleanup

--- test.py
+++
@@ -1,5 +0,0 @@
-from functools import reduce
-
-d = {'a': {'b': {'c': {"value": "ll"}}, 'e': 'aaaa'}}
-v = reduce(lambda di, key: di.get(key, {}) if di and isinstance(di, dict) else None, ['a', 'b'], d)
-print(v)
